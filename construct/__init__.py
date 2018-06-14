25c1d4492eb4bddfecd389aae416456623bfb8dd
Dan Bradham <danielbradham@gmail.com>
2018-06-14 17:55:06 -0400

    Release 0.1.21

--- construct/__init__.py
+++ construct/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 
 __title__ = 'construct'
 __description__ = 'Creative project and asset management'
-__version__ = '0.1.20'
+__version__ = '0.1.21'
 __author__ = 'Dan Bradham'
 __email__ = 'danielbradham@gmail.com'
 __license__ = 'MIT'
