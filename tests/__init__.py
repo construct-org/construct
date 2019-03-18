acf0c5c4c49586cecbf0b080ed3536628647d834
Dan Bradham <danielbradham@gmail.com>
2019-03-18 13:22:35 -0400

    Use pathlib in tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 import os
-
+from construct.utils import unipath
 
 this_dir = os.path.abspath(os.path.dirname(__file__))
 
 
 def test_dir(*paths):
-    return os.path.join(this_dir, *paths).replace('\\', '/')
+    return unipath(this_dir, *paths)
 
 
 def data_dir(*paths):
