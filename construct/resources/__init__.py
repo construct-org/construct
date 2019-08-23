cd2967e02253b36b16555beb38594746ae320621
Dan Bradham <danielbradham@gmail.com>
2019-08-23 18:46:51 -0400

    Create Resource object

--- construct/resources/__init__.py
+++ construct/resources/__init__.py
@@ -1,55 +1,122 @@
 # -*- coding: utf-8 -*-
+
+# Standard library imports
 from __future__ import absolute_import
 import os
 import re
 import glob
 import logging
+import inspect
 
+# Local imports
 from ..utils import unipath
 from ..ui.scale import pix
+from ..compat import wraps, Path
 
 
 _log = logging.getLogger('construct.ui.resources')
-_initialized = False
-_font_ids = []
 
-EXTENSIONS = ['', '.png', '.ttf', '.css']
-THIS_PATH = unipath(os.path.dirname(__file__))
+RESOURCE_EXTENSIONS = ['', '.png', '.ttf', '.css']
+this_package = Path(__file__).parent
 
 
-def load():
-    '''Load compiled qt resources and add application fonts.'''
-
-    global _initialized
-    if _initialized:
-        return
-
-    # Load built resources
-    from . import _resources
-
-    # Load fonts
-    from Qt import QtGui
-    for resource, resource_file in ls('fonts'):
-        font_id = QtGui.QFontDatabase.addApplicationFont(resource)
-        _font_ids.append(font_id)
-
-    _initialized = True
-
-
-def unload():
-    # TODO: we could unload fonts here, but should we bother?
-    pass
+def ensure_resources_loaded(fn):
+    '''A decorator that executes callables that have been marked lazy using
+    the lazy function.
+    '''
+    @wraps(method)
+    def call_fn(*args, **kwargs):
+        load_qt_resources()
+        return call_fn(*args, **kwargs):
+    return call_fn
 
 
 class ResourceNotFoundError(Exception):
     '''Raised when a resource can not be found.'''
-    pass
 
 
-def path(resource):
+class Resources(object):
+    '''Resources object. Looks up builtin qt resources as well as user
+    resources stored in a users CONSTRUCT_PATH.'''
+
+    _qresources_loaded = False
+    _font_ids = []
+
+    def load(self):
+        pass
+
+    def unload(self):
+        pass
+
+    def find(self, resource):
+        try:
+            return find(resource)
+        except ResourceNotFoundError:
+            return self.api.path.find(resources.lstrip(':/'))
+
+    def ls(self, search=None):
+
+        builtin_resources = ls(search)
+        user_resources = []
+
+        for path in self.api.path:
+            user_resources.extend(ls(search, path=path))
+
+        return builtin_resources + user_resources
+
+    def read(self, resource):
+        '''Read a resource'''
+
+        return self.find(resource).read_text(encoding='utf-8')
+
+    @ensure_resources_loaded
+    def style(self, name_or_resource):
+        '''Get a stylesheet from a name or resource.'''
+
+        if name_or_resource.startswith(':/'):
+            resource = name_or_resource
+        else:
+            resource = ':/css/' + name_or_resource + '.css'
+        style = self.read(resource)
+
+        pixel_values = re.findall(r'\d+px', style)
+        for pixel_value in pixel_values:
+            scaled_value = pix(int(pixel_value[:-2]))
+            style.replace(pixel_value, str(scaled_value) + 'px', 1)
+
+        return style
+
+    @ensure_resources_loaded
+    def icon(resource, size=None, color=None):
+        '''Get a resource as a QIcon'''
+        pass
+
+    @ensure_resources_loaded
+    def pixmap(resource, size=None, color=None):
+        '''Get a resource as a QPixmap'''
+        pass
+
+
+def load():
+    if Resources._qresources_loaded:
+        return
+
+    _log.debug('Loading Qt resources.')
+    from . import _resources
+
+    _log.debug('Loading fonts.')
+    from Qt import QtGui
+    for resource, resource_file in ls('fonts'):
+        font_id = QtGui.QFontDatabase.addApplicationFont(resource)
+        Resources._font_ids.append(font_id)
+
+    Resources._qresources_loaded = True
+
+
+def find(resource):
     '''Get a resources filepath'''
 
-    resource_file = THIS_PATH / resource.lstrip(':/')
+    resource_file = this_package / resource.lstrip(':/')
 
     if not resource_file.is_file():
         raise ResourceNotFoundError('Could not find resource: ' + resource)
@@ -57,24 +124,29 @@ def path(resource):
     return resource_file
 
 
-def ls(search=None):
-    '''List all resources
+def ls(search=None, path=None, extensions=RESOURCE_EXTENSIONS):
+    '''List all resources.
+
+    Arguments:
+        search (str): String to search for
+        path (Path): Directory to search for resources
 
     Returns:
         List of tuples - (resource, resource_file)
     '''
-    resources = []
+    path = path or this_package
+    resources = {}
 
-    for file in THIS_PATH.glob('*/*.*'):
+    for file in path.glob('*/*.*'):
 
-        if file.suffix not in EXTENSIONS:
+        if file.suffix not in extensions:
             continue
 
         if search and search not in file.as_posix():
             continue
 
-        resource = ':/' + file.relative_to(THIS_PATH).as_posix().lstrip('./')
-        resources.append((resource, file.as_posix()))
+        resource = ':/' + file.relative_to(path).as_posix().lstrip('./')
+        resources[resource] = file.as_posix()
 
     return resources
 
